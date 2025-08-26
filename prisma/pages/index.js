15e253587949248ca8b859d66ffb8c90ef27768f
XxchovxX <59129133+XxchovxX@users.noreply.github.com>
2025-08-26 10:09:47 -0500

    Create index.js

---
+++ prisma/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1>¡Bienvenido a la Intranet Municipal!</h1>
+      <p>Este es el punto de partida. Poco a poco iremos añadiendo módulos.</p>
+    </div>
+  );
+}
